7d29ab7db63f993ff4a927fec3a67ed2c43179c7
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-10-30 11:13:49 +0800

    C Study

--- Project25/1.C
+++ Project25/1.C
@@ -2,22 +2,69 @@
 
 #include<stdio.h>
 
-#include<windows.h>
 int main()
 {
-	int i = 0;
-	int count = 0;
-	for (i = 0; i < 100; i++)
-	{
-		printf("%5d", i);
-		count++;
-		if (10 == count)
-		{
-			printf("\n");
-			count = 0;
-		}
-	}
-	system("pause");
+
 
 	return 0;
 }
+
+////#include<string.h>
+//#include<assert.h>
+//void my_strcpy(char* dest,const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[] = "############";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+////int Add(int x, int y)
+////{
+////	return x + y;
+////}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	//i = Add(i, 0);  
+//	for (i = 0; i <= 12;  i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//
+//	return 0;
+//}
+
+//#include<windows.h>
+//int main()
+//{  
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%5d", i);
+//		count++;
+//		if (10 == count)
+//		{
+//			printf("\n");
+//			count = 0;
+//		}
+//	}
+//	system("pause");
+//
+//	return 0;
+//}

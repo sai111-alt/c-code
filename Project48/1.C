8fc7af6788312ea8e731c63323c6b5a1611a319e
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-10-31 19:55:34 +0800

    C STUDY

---
+++ Project48/1.C
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+
+	return 0;
+}
+
+//#include<assert.h>
+//char* my_strcat(char* dest, char* src)
+//{
+//	char* ret = dest;
+//	assert(*dest && *src);
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++);
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[30] = "hello";
+//	char arr2[] = "world";
+//	my_strcat(arr1, arr2);
+//	printf("%s", arr1);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	if (strlen("abc") - strlen("abcde") > 0)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//
+//	return 0;
+//}

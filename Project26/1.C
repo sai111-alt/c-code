96fb09295a6503b9d2848e6a2859f08822da15ba
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-08 15:55:49 +0800

    C Study

--- Project26/1.C
+++ Project26/1.C
@@ -2,12 +2,81 @@
 
 #include<stdio.h>
 
-int main()
+int mian()
 {
-	int a = 20;
+
 
 	return 0;
 }
+
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d\n", a, b, c);
+//
+//	return 0;
+//}
+
+//int check_sys()
+//{
+//	int a = 1;
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("小端\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 20;
+//
+//	return 0;
+//}
 
 //void test(void)
 //{

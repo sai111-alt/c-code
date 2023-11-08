f86cf943b3fde66b29842ebe2f6937301804b800
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-08 17:49:50 +0800

    C Study

--- Project26/1.C
+++ Project26/1.C
@@ -2,12 +2,84 @@
 
 #include<stdio.h>
 
-int mian()
+int main()
 {
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
 
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
 
 	return 0;
 }
+
+//int main()
+//{
+//	double d = 1E10;
+//	printf("%lf\n", d);
+//
+//	return 0;
+//}
+
+//#include<Windows.h>
+//int main()
+//{
+//	unsigned char i = 0;
+//	for (i = 0; i <= 255; i++)
+//	{
+//		Sleep(100);
+//		printf("hello world\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = 48;
+//	printf("%c\n", a);
+//
+//	return 0;
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));
+//
+//	return 0;
+//}
+
+//#include<Windows.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(100);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//
+//	return 0;
+//}
 
 //int main()
 //{

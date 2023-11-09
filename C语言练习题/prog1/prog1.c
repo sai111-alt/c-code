1cf9cd79950e9c432a434d7a0a78fff089a5b850
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-09 08:03:57 +0800

    C Practice

---
+++ C语言练习题/prog1/prog1.c
@@ -0,0 +1,114 @@
+#include <stdio.h> 
+#include <string.h>
+#pragma warning (disable:4996)
+void fun(char*sp ,int *ct)
+{
+	//老师的方法
+	int i = 0;
+	int len = strlen(sp);
+	for (i = 0; i < 3; i++)
+	{
+		ct[i] = 0;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if ((sp[i] == 'e') && (sp[i + 1] == 'a'))
+		{
+			ct[0]++;
+		}
+		else if ((sp[i] == 'o') && (sp[i + 1] == 'u'))
+		{
+			ct[1]++;
+		}
+		else if ((sp[i] == 'i') && (sp[i + 1] == 'u'))
+		{
+			ct[2]++;
+		}
+	}
+
+
+	//我的方法
+	//char* i = "ea";
+	//char* j = "ou";
+	//char* k = "iu";
+	//ct[0] = 0;
+	//ct[1] = 0;
+	//ct[2] = 0;
+
+	//char* cur = sp;
+	//char* s1 = NULL;
+	//char* s2 = NULL;
+
+	//while (*cur)
+	//{
+	//	s1 = cur;
+	//	s2 = i;
+	//	while (s1 && s2 && (*s1 == *s2))
+	//	{
+	//		s1++;
+	//		s2++;
+	//	}
+	//	if (!*s2)
+	//	{
+	//		ct[0]++;
+	//	}
+	//	cur++;
+	//}
+	//cur = sp;
+	//while (*cur)
+	//{
+	//	s1 = cur;
+	//	s2 = j;
+	//	while (*s1 && *s2 && (*s1 == *s2))
+	//	{
+	//		s1++;
+	//		s2++;
+	//	}
+	//	if (!*s2)
+	//	{
+	//		ct[1]++;
+	//	}
+	//	cur++;
+	//}
+	//cur = sp;
+	//while (*cur)
+	//{
+	//	s1 = cur;
+	//	s2 = k;
+	//	while (*s1 && *s2 && (*s1 == *s2))
+	//	{
+	//		s1++;
+	//		s2++;
+	//	}
+	//	if (!*s2)
+	//	{
+	//		ct[2]++;
+	//	}
+	//	cur++;
+	//}
+}
+main()
+{	void NONO();
+	char txt[200]="abeaeafeeaoukgdoouuoiouifwieeotiu";
+	int c[3];
+	fun(txt,c);
+	printf("%d,%d,%d\n",c[0],c[1],c[2]); 
+	NONO();
+}
+void NONO ()
+{/* 请在此函数内打开文件，输入测试数据，调用 fun 函数，输出数据，关闭文件。 */
+  FILE *rf, *wf ;
+  int i, c[3] ;
+  char txt[200], *p ;
+
+  rf = fopen("in.dat","r") ;
+  wf = fopen("out.dat","w") ;
+  for(i = 0 ; i < 10 ; i++) {
+    fgets(txt, 200, rf) ;
+    p = strchr(txt, '\n') ;
+    if(p != NULL) *p = 0 ;
+	fun(txt,c);
+	fprintf(wf, "%d,%d,%d\n",c[0],c[1],c[2]); 
+  }
+  fclose(rf) ; fclose(wf) ;
+}

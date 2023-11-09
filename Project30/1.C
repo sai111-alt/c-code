3140ea20fda073e1c6aa5aa1be96eef84413352f
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-09 22:14:51 +0800

    C Study

--- Project30/1.C
+++ Project30/1.C
@@ -2,14 +2,67 @@
 
 #include<stdio.h>
 
-int main()
-{
-
-
-	return 0;
-}
+//void print1(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void print2(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			//printf("%d ", *((*arr+i) + j));
+//			printf("%d ", (*arr+i)[j]);
+//		}
+//		printf("\n"); 
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6} ,{3,4,5,6,7} };
+//	print1(arr, 3, 5);
+//	printf("\n");
+//	print2(arr, 3, 5);
+//
+//	return 0;
+//}
 
 //int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		//printf("%d ", *(*pa + i));
+//		printf("%d ", (*pa)[i]);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* arr[5];
+//	char*(*pa)[5] = &arr;
+//
+//	return 0;
+//}
+
+//int main ()
 //{
 //	int arr1[] = { 1,2,3,4,5 };
 //	int arr2[] = { 2,3,4,5,6 };

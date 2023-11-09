e4d294f4bed9561c9865a45a00eb5d5fff368cb7
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-09 16:57:54 +0800

    C Study

--- Project26/1.C
+++ Project26/1.C
@@ -2,19 +2,26 @@
 
 #include<stdio.h>
 
-int main()
-{
-	int n = 9;
-	float* pFloat = (float*)&n;
-	printf("n的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
+//int main()
+//{
+//	float f = 5.5;
+//
+//	return 0;
+//}
 
-	*pFloat = 9.0;
-	printf("num的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
-
-	return 0;
-}
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//
+//	return 0;
+//}
 
 //int main()
 //{
---
+++ Project30/1.C
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+
+
+	return 0;
+}
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//	
+//	int* parr[] = { arr1,arr2,arr3 };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", *(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* p = "abcdef";
+//	*p = 'W';
+//	printf("%s\n", p);
+//
+//	return 0;
+//}

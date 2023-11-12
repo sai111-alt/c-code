b95d96ed1a0c70a8ee88c014726795bee9127c06
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-12 17:30:46 +0800

    C Study

--- Project54/1.C
+++ Project54/1.C
@@ -2,9 +2,30 @@
 
 #include<stdio.h>
 
+struct s1
+{
+	char c1;
+	int a;
+	char c2;
+};
+struct s2
+{
+	char c1;
+	char c2;
+	int a;
+};
 int main()
 {
-
+	struct s1 a = { 0 };
+	struct s2 b = { 0 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(b));
 
 	return 0;
 }
+
+//typedef struct Node
+//{
+//	int data;
+//	struct Node* next;
+//}Node;

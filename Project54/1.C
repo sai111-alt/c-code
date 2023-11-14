dd490a7c6f8511f3d74ed862e4df4d94ca75512e
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-14 20:57:12 +0800

    C Study

--- Project54/1.C
+++ Project54/1.C
@@ -2,27 +2,49 @@
 
 #include<stdio.h>
 
-struct s1
-{
-	char c1;
-	int a;
-	char c2;
-};
-struct s2
-{
-	char c1;
-	char c2;
-	int a;
-};
-int main()
-{
-	struct s1 a = { 0 };
-	struct s2 b = { 0 };
-	printf("%d\n", sizeof(a));
-	printf("%d\n", sizeof(b));
+//struct s3
+//{
+//	double d;
+//	char c;
+//	int i;
+//};
+//struct s4
+//{
+//	char c1;
+//	struct s3 s3;
+//	double d;
+//};
+//int main()
+//{
+//	struct s3 r;
+//	struct s4 i;
+//	printf("%d\n", sizeof(r));
+//	printf("%d\n", sizeof(i));
+//
+//	return 0;
+//}
 
-	return 0;
-}
+//struct s1
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//};
+//struct s2
+//{
+//	char c1;
+//	char c2;
+//	int a;
+//};
+//int main()
+//{
+//	struct s1 a = { 0 };
+//	struct s2 b = { 0 };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(b));
+//
+//	return 0;
+//}
 
 //typedef struct Node
 //{

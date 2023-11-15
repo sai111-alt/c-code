3cc4e4c2f56ab94ed0f9c239f08443b29b753964
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2023-11-15 16:24:57 +0800

    C Study

--- Project54/1.C
+++ Project54/1.C
@@ -2,6 +2,111 @@
 
 #include<stdio.h>
 
+int main()
+{
+
+
+	return 0;
+}
+
+//struct S
+//{
+//	char a : 3;
+//	char b : 4;
+//	char c : 5;
+//	char d : 4;
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	s.a = 10;
+//	s.b = 12;
+//	s.c = 3;
+//	s.d = 4;
+//
+//	return 0;
+//}
+//
+//struct S
+//{
+//	int a : 2;
+//	int b : 4;
+//	int c : 10;
+//	int d : 30;
+//};
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));
+//
+//	return 0;
+//}
+
+//struct s
+//{
+//	int data;
+//	int num;
+//};
+//void print1(struct s s)
+//{
+//	printf("%d,%d\n", s.data, s.num);
+//}
+//void print2(struct s* ps)
+//{
+//	printf("%d,%d\n", ps->data, ps->num);
+//}
+//int main()
+//{
+//	struct s s = { 0,2 };
+//	print1(s);
+//	print2(&s);
+//
+//	return 0;
+//}
+
+
+//#pragma pack(8)
+//struct s1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//#pragma pack()
+//
+//#pragma pack(1)
+//struct s2
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//#pragma pack()
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct s1));
+//	printf("%d\n", sizeof(struct s2));
+//
+//	return 0;
+//}
+
+//#include<stddef.h>
+//struct s
+//{
+//	char c;
+//	int i;
+//	double d;
+//};
+//int main()
+//{
+//	printf("%d\n", offsetof(struct s, c));
+//	printf("%d\n", offsetof(struct s, i));
+//	printf("%d\n", offsetof(struct s, d));
+//
+//	return 0;
+//}
+
 //struct s3
 //{
 //	double d;

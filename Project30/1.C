78f9091053e2f897d3f3e78cfbce1f0eb36c772e
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-01 16:33:18 +0800

    C Practice

--- Project30/1.C
+++ Project30/1.C
@@ -2,9 +2,71 @@
 
 #include<stdio.h>
 
+//#include<stdlib.h>
+//void buddle_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+void bubble_sort(void* base, int sz, int width, int (*cmp)(void* e1, void* e2))
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (cmp((char*)base + j * width , (char*)base + (j + 1) * width) > 0)
+			{
+				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+
 int main()
 {
+	int arr[10] = { 8,9,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[9]);
+	//buddle_sort(arr, sz);
+	//qsort(arr, sz, 4, cmp_int);
 
+	bubble_sort(arr, sz, sizeof(arr[0]),cmp_int);
+
+
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 
 	return 0;
 }

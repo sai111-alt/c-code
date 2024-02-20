ea8372c3e1f5d811a1491558bbaac96d85017058
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-20 17:08:57 +0800

    C Practice

--- Project48/1.C
+++ Project48/1.C
@@ -3,30 +3,85 @@
 #include<stdio.h>
 #include<string.h>
 
-#include<assert.h>
-void* my_memcpy(void* dest,const void* src,int num)
-{
-	void* ret = dest;
-	assert(dest != NULL);
-	assert(src != NULL);
-	while (num--)
-	{
-		*(char*)dest = *(char*)src;
-		++(char*)dest;
-		++(char*)src;
-	}
-	return ret;
-}
 int main()
 {
-	int arr1[] = { 1,2,3,4,5 };
-	int arr2[5] = { 0 };
-
-	//memcpy(arr2, arr1, sizeof(arr1));
-	my_memcpy(arr2, arr1, sizeof(arr1));
+	char arr[10] = "";
+	memset(arr, '#', 10);
+	int arr1[10] = { 0 };
+	memset(arr1, 1, 10);
 
 	return 0;
 }
+
+//int main()
+//{
+//	int arr1[10] = {1,2,3,4,5};
+//	int arr2[10] = {1,2,5,4,3};
+//	int ret = memcmp(arr1, arr2, 9);
+//
+//	return 0;
+//}
+
+//#include<assert.h>
+//void* my_memmove(void* dest, const void* src, size_t count)
+//{
+//	void* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	if (dest < src)
+//	{
+//		while (count--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			++(char*)dest;
+//			++(char*)src;
+//		}
+//	}
+//	else
+//	{
+//		while (count--)
+//		{
+//			*((char*)dest + count) = *((char*)src + count);
+//		}
+//	}
+//
+//	return ret;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	//memcpy(arr + 2, arr, 20);
+//	//memmove(arr + 2, arr, 20);
+//	my_memmove(arr, arr + 2, 20);
+//
+//	return 0;
+//}
+
+//#include<assert.h>
+//void* my_memcpy(void* dest,const void* src,int num)
+//{
+//	void* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (num--)
+//	{
+//		*(char*)dest = *(char*)src;
+//		++(char*)dest;
+//		++(char*)src;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[5] = { 0 };
+//
+//	//memcpy(arr2, arr1, sizeof(arr1));
+//	my_memcpy(arr2, arr1, sizeof(arr1));
+//
+//	return 0;
+//}
 
 //int main()
 //{

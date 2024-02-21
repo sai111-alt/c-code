d7ee85e5230bdda24e95daccd1c4fa49f64e7e2d
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-21 23:01:52 +0800

    C Practice

--- Project70/1.C
+++ Project70/1.C
@@ -1,1 +1,206 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<errno.h>
+#include<string.h>
+
+int main()
+{
+	FILE* pf = fopen("test.txt", "r");
+	if (pf == NULL)
+	{
+		return 0;
+	}
+	//fseek(pf, -2, SEEK_END);
+	//int ch = fgetc(pf);
+	//printf("%c", ch);
+	int ch=fgetc(pf);
+	printf("%c\n", ch);
+	
+	rewind(pf);
+
+	ch = ftell(pf);
+	printf("%d", ch);
+
+	fclose(pf);
+	pf = NULL;
+
+	return 0;
+}
+
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//int main()
+//{
+//	//struct S s = { "ÕÅÈý",20,55.6 };
+//	FILE* pf = fopen("test.txt", "rb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	struct S b = { 0 };
+//
+//	//fwrite(&s, sizeof(struct S), 1, pf);
+//
+//	fread(&b, sizeof(struct S), 1, pf);
+//	printf("%s %d %lf", b.name, b.age, b.score);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct s
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//int main()
+//{
+//	//struct s s = { 100,3.14f, "bit" };
+//	//FILE* pf = fopen("test.txt", "r");
+//	//if (pf == NULL)
+//	//{
+//	//	return 0;
+//	//}
+//
+//	//fscanf(pf,"%d %f %s", &(s.n), &(s.score), s.arr);
+//	//printf("%d %f %s", s.n, s.score, s.arr);
+//
+//	//fclose(pf);
+//	//pf = NULL;
+//
+//	struct s s = { 0 };
+//	fscanf(stdin, "%d %f %s", &(s.n), &(s.score), s.arr);
+//	fprintf(stdout, "%d %.2f %s", s.n, s.score, s.arr);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char buf[1024] = { 0 };
+//	//fgets(buf, 1024, stdin);
+//	//fputs(buf, stdout);
+//
+//	gets(buf);
+//	puts(buf);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char buf[1024] = { 0 };
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//
+//	fputs("hello\n", pf);
+//	fputs("world\n", pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char buf[1024] = { 0 };
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//
+//	fgets(buf, 1024, pf);
+//	printf("%s", buf);
+//
+//	fgets(buf, 1024, pf);
+//	printf("%s", buf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = fgetc(stdin);
+//	fputc(ch, stdout);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//
+//	printf("%c", fgetc(pf));
+//	printf("%c", fgetc(pf));
+//	printf("%c", fgetc(pf));
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	fputc('s', pf);
+//	fputc('a', pf);
+//	fputc('i', pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	
+//	FILE* pf = fopen("test.txt", "a");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10000;
+//	FILE* pf = fopen("test.txt", "wb");
+//	fwrite(&a, 4, 1, pf);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}

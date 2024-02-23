681f2ae5bb47668f3f68fe3fb9e8bae2f36132ba
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-23 15:11:53 +0800

    C Practice

--- Project76/Project76/1.C
+++ Project76/Project76/1.C
@@ -1,8 +1,85 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include<stdio.h>
+
 int main()
 {
 
 
 	return 0;
 }
+
+//#define SQUARE(x) ((x)*(x))
+//int main()
+//{
+//	int ret = SQUARE(5);
+//	printf("%d %d", ret,SQUARE(5+1));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d", __STDC__);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror(" ");
+//		return 0;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		fprintf(pf,"file:%s line:%d  date:%s time:%s\n",
+//			__FILE__, __LINE__, __DATE__, __TIME__);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);
+//
+//	return 0;
+//}
+
+//extern int Add(int x, int y);
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	int c = Add(a, b);
+//	printf("\nc = %d", c);
+//
+//	return 0;
+//}

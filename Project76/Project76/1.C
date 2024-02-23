f1c1f32295bccf46d002158344faec22240494d6
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-23 21:48:42 +0800

    C Practice

--- Project76/Project76/1.C
+++ Project76/Project76/1.C
@@ -2,12 +2,101 @@
 
 #include<stdio.h>
 
+#include<stddef.h>
+typedef struct s
+{
+	char c1;
+	int a;
+	char c2;
+}S;
+#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
 int main()
 {
+	S s = {1,2,3};
+	int c = sizeof(s);
+	//printf("%d\n", c);
 
+	//printf("%d\n", offsetof(S, c1));
+	//printf("%d\n", offsetof(S, a));
+	//printf("%d\n", offsetof(S, c2));
+
+	printf("%d\n", OFFSETOF(S, c1));
+	printf("%d\n", OFFSETOF(S, a));
+	printf("%d\n", OFFSETOF(S, c2));
+
+	printf("%d %d %d", s.c1, s.a, s.c2);
 
 	return 0;
 }
+
+//#define ABC
+//int main()
+//{
+//	int a = 10;
+//#ifndef ABC
+//	printf("%d\n", a);
+//#endif
+//
+//#if !defined(ABC)
+//	printf("%d\n", a);
+//#endif
+//
+//#if 1+1
+//	printf("hehe");
+//#endif
+//
+//	return 0;
+//}
+
+//#define MAX 100
+//int main()
+//{
+//	printf("MAX=%d\n", MAX);
+//
+//#undef MAX
+//
+//	printf("MAX=%d\n", MAX);
+//	 
+//
+//	return 0;
+//}
+
+//#define MAX(X,Y) ((X)>(Y)?(X):(Y))
+//int main()
+//{
+//	int a = 10;
+//	int b = 11;
+//	
+//	int max = MAX(a++, b++);
+//
+//	printf("%d %d %d", a, b, max);
+//
+//	return 0;
+//}
+
+//#define PRINT(X) printf("the value of "#X" is %d\n",X)
+//#define CAT(X,Y) X##Y
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int Class84 = 2024;
+//	 
+//	PRINT(a);
+//	PRINT(b);
+//	printf("%d\n", CAT(Class, 84));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hello world\n");
+//	printf("hello ""world\n");
+//	printf("hel""lo"" world\n");
+//
+//	return 0;
+//}
 
 //#define SQUARE(x) ((x)*(x))
 //int main()

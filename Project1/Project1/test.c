cbf390e1f72c82705a5b393eee0cfe062231875d
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-24 09:41:15 +0800

    C Practice

--- Project1/Project1/test.c
+++ Project1/Project1/test.c
@@ -2,20 +2,27 @@
 
 #include <stdio.h>
 
+int main()
+{
+	printf("%s will come tonight", "张三");
+
+	return 0;
+}
+
 //int global = 100;//—全局变量
 
-int main()//主函数-程序的入口-main函数有且只有一个
-{
+//int main()//主函数-程序的入口-main函数有且只有一个
+//{
 	//extern int g_val;//extern——声明一下外部符号
 	//printf("g_val=%d\n", g_val);
 
 	//计算两个数的和
-	int num1 = 0;
-	int   num2 = 0;
-	int sum = 0;	
-	scanf("%d%d",&num1,&num2);
-	sum = num1 + num2;
-	printf("sum = %d\n", sum);
+	//int num1 = 0;
+	//int   num2 = 0;
+	//int sum = 0;	
+	//scanf("%d%d",&num1,&num2);
+	//sum = num1 + num2;
+	//printf("sum = %d\n", sum);
 	
 	//int global = 10;//——局部变量 
 	//printf("global = %d\n", global);
@@ -47,5 +54,5 @@ int main()//主函数-程序的入口-main函数有且只有一个
 
 	//printf("hehe world"); 
 
-		return 0;
-}
+		//return 0;
+//}

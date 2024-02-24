f48dc1f85aa95fc1f191a30730a3d93ae1d64ec8
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-24 11:27:30 +0800

    C Practice

--- Project1/Project1/test.c
+++ Project1/Project1/test.c
@@ -4,10 +4,72 @@
 
 int main()
 {
-	printf("%s will come tonight", "张三");
+	//char a = 0;
+	//scanf(" %c", &a);
+	//printf("%casdf", a);
 
+	//char arr[10] = { 0 };
+	//scanf("%5s", arr);
+	//printf("%s", arr);
+
+	int year = 0;
+	int month = 0;
+	int day = 0;
+
+	scanf("%d%*c%d%*c%d", &year, &month, &day);
+	printf("%d %d %d", year, month, day);
+	
 	return 0;
 }
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	while (scanf("%d%d", &a, &b) != EOF)
+//	{
+//		int c = a + b;
+//		printf("%d\n", c);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x;
+//	float y;
+//
+//	scanf("%d%f", &x, &y);
+//	printf("%d %f\n", x, y);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i, j, k, l=0;
+//	int ret = 0;
+//	ret = scanf("%d%d%d%d", &i, &j, &k, &l);
+//
+//	printf("%d\n%d\n%d\n%d\n%d\n", i, j, k, l, ret);
+//
+// 	return 0;
+//}
+
+//int main()
+//{
+//	printf("%s will come%% tonight\n", "张三");
+//	printf("%+d\n", 5);
+//	printf("%+d\n", -5);
+//	printf("%.2f\n", 5.223);
+//	printf("%.2f\n", 5.226);
+//	printf("%6.2f\n", 5.226);
+//	printf("%*.*f\n", 6, 2, 5.226);
+//	printf("%.4s\n", "hello");
+//
+//	return 0;
+//}
 
 //int global = 100;//—全局变量
 

2a8242fd3f5d64442624de32b1dac39dfe740295
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-02-26 10:28:26 +0800

    C Practice

--- Address book/Address book/contact.h
+++ Address book/Address book/contact.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include<stdio.h>
+
+#define MAX 1000
+
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 20
+
+struct PenInof
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
--- Address book/Address book/test.c
+++ Address book/Address book/test.c
@@ -2,9 +2,60 @@
 
 #include"contact.h"
 
+//function：
+//1.可以存放1000个好友的信息，包括名字、电话、性别、住址、年龄
+//2.可以增加好友信息
+//3.可以删除指定名字的好友信息
+//4.可以查找好友信息
+//5.可以修改好友信息
+//6.可以显示好友信息
+//7.可以排序好友信息
+
+void menu()
+{
+	printf("*********************************************\n");
+	printf("******      1.add            2.del     ******\n");
+	printf("******      3.search         4.modify  ******\n");
+	printf("******      5.show           6.sort    ******\n");
+	printf("******      0.exti                     ******\n");
+	printf("*********************************************\n");
+
+}
+
 int main()
 {
+	int input = 0;
 
+	struct PenInof con[MAX];
+
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 0:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+
+	}while(input);
 
 	return 0;
 }

3e1fb3cfa835229128ba2fd700864358f4138368
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-05-11 17:31:29 +0800

    Snake

---
+++ SnakeNode/SnakeNode/1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	system("mode con cols=100 lines=30");
+	system("title Ã∞≥‘…ﬂ");
+	system("pause");
+
+	return 0;
+}
